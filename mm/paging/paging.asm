ca755d8077cdd88c591da353320cc8dfbf3a89ed
Sara <yojan.bustamante@udea.edu.co>
2022-11-02 18:56:47 -0500

    derp

--- mm/paging/paging.asm
+++ mm/paging/paging.asm
@@ -8,20 +8,20 @@
 paging_load_directory:
 	push ebp
 	mov ebp, esp
-
+	
 	mov eax, [ebp+8]
 	mov cr3, eax
-
+	
 	pop ebp
 	ret
 
 paging_enable:
 	push ebp
 	mov ebp, esp
-
+	
 	mov eax, cr0
 	or eax, 0x80000000
-	mov eax, cr0
-
+	mov cr0, eax
+	
 	pop ebp
 	ret
